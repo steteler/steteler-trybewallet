2b01ade8f3a398ba08a9ea06084b6cc1c053c85f
kikutii <joaovictorkikuti@gmail.com>
2022-04-28 15:59:24 -0300

    add new Component 'Header'

---
+++ src/Components/Header.jsx
@@ -0,0 +1,28 @@
+import propTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: propTypes.string.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    email: state.user.email,
+  };
+}
+
+export default connect(mapStateToProps)(Header);
