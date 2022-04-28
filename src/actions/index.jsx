ab400a6e9168a873c6c1976257152f02b1eb6cdb
kikutii <joaovictorkikuti@gmail.com>
2022-04-28 15:56:08 -0300

    add new Action 'saveWallet' to save currencies and expenses

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,11 +1,18 @@
-// Coloque aqui suas actions
-
 export const INPUT_LOGIN = 'INPUT_LOGIN';
+export const INPUT_WALLET = 'INPUT_WALLET';
 
 export function saveLogin(email, password) {
   return {
     type: INPUT_LOGIN,
     email,
     password,
+  };
+}
+
+export function saveWallet(currencies, expenses) {
+  return {
+    type: INPUT_WALLET,
+    currencies,
+    expenses,
   };
 }
