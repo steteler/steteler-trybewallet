c2d2d647f03e24dfec83a79113edefb10655d993
kikutii <joaovictorkikuti@gmail.com>
2022-04-28 15:58:15 -0300

    add new Reducer 'wallet' to update currencies and expenses from Redux state

--- src/reducers/wallet.jsx
+++ src/reducers/wallet.jsx
@@ -1,1 +1,22 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { INPUT_WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, { type, currencies, expenses }) {
+  switch (type) {
+  case INPUT_WALLET:
+    return {
+      ...state,
+      currencies,
+      expenses,
+    };
+
+  default:
+    return state;
+  }
+}
+
+export default wallet;
