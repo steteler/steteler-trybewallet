0548c2a4a338a526996404d29146ca66c8cf97d7
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 16:14:44 -0300

    add all buttons in wallet

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,4 +1,4 @@
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { saveWallet } from '../actions';
@@ -12,17 +12,89 @@ class Wallet extends React.Component {
   }
 
   render() {
+    const { currencies } = this.props;
     return (
       <>
         <Header />
-        <div>Wallet</div>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input
+              id="value"
+              placeholder="Valor"
+              data-testid="value-input"
+            />
+          </label>
+
+          <label htmlFor="currency">
+            Moeda:
+            <select
+              id="currency"
+              data-testid="currency-input"
+            >
+              {
+                currencies.map((currency) => (
+                  <option
+                    key={ currency }
+                    value={ currency }
+                  >
+                    { currency }
+                  </option>
+                ))
+              }
+            </select>
+          </label>
+
+          <label htmlFor="method">
+            Método de Pagamento:
+            <select
+              id="method"
+              data-testid="method-input"
+            >
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="category">
+            Categoria:
+            <select
+              id="category"
+              data-testid="tag-input"
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+
+          <label htmlFor="description">
+            Descrição:
+            <input
+              id="description"
+              placeholder="Descrição"
+              data-testid="description-input"
+            />
+          </label>
+          <input type="submit" value="Adicionar" />
+        </form>
       </>
     );
   }
 }
 
 Wallet.propTypes = {
-  dispatch: propTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default connect()(Wallet);
+function mapStateToProps(state) {
+  return {
+    currencies: state.wallet.currencies,
+  };
+}
+
+export default connect(mapStateToProps)(Wallet);
