12adb42428d4aa36596e5ed6a031489b941aeca7
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 02:29:29 -0300

    return Object keys from API and delete the specific key 'USDT'

--- src/services/getCurrencies.jsx
+++ src/services/getCurrencies.jsx
@@ -1,7 +1,8 @@
 async function getCurrencies() {
   const results = await fetch('https://economia.awesomeapi.com.br/json/all');
   const currencies = await results.json();
-  return currencies;
+  delete currencies.USDT;
+  return Object.keys(currencies);
 }
 
 export default getCurrencies;
