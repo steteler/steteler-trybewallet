db842f5ccb7da9bcbcfd0349db0627e496cdc60b
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 20:40:07 -0300

    inputs of the Page Wallets finished

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,44 +1,95 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { saveWallet } from '../actions';
+import { saveCurrencies, saveWallet } from '../actions';
 import Header from '../Components/Header';
 import getCurrencies from '../services/getCurrencies';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: 0,
+      value: 0,
+      currency: 'USD',
+      method: '',
+      tag: '',
+      description: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.saveOnRedux = this.saveOnRedux.bind(this);
+    this.resetInputs = this.resetInputs.bind(this);
+  }
+
   async componentDidMount() {
     const { dispatch } = this.props;
-    dispatch(saveWallet(await getCurrencies()));
+    dispatch(saveCurrencies(
+      Object.keys(await getCurrencies()),
+    ));
+  }
+
+  onInputChange({ target: { id, value } }) {
+    this.setState({ [id]: value });
+  }
+
+  async saveOnRedux() {
+    const { dispatch } = this.props;
+    const exchangeRates = await getCurrencies();
+    const obj = { ...this.state, exchangeRates };
+    dispatch(saveWallet(obj));
+    this.resetInputs();
+    this.setState((prevState) => (
+      { id: prevState.id + 1 }
+    ));
+  }
+
+  resetInputs() {
+    this.setState({
+      value: 0,
+      currency: 'USD',
+      method: '',
+      tag: '',
+      description: '',
+    });
   }
 
   render() {
     const { currencies } = this.props;
+    const { value, currency, method, tag, description } = this.state;
     return (
       <>
         <Header />
-        <form>
+        <section>
           <label htmlFor="value">
             Valor:
             <input
+              value={ value }
               id="value"
+              type="number"
               placeholder="Valor"
               data-testid="value-input"
+              onChange={ this.onInputChange }
+              min="0"
             />
           </label>
 
           <label htmlFor="currency">
             Moeda:
             <select
+              value={ currency }
               id="currency"
               data-testid="currency-input"
+              onChange={ this.onInputChange }
             >
               {
-                currencies.map((currency) => (
+                currencies.map((coin) => (
                   <option
-                    key={ currency }
-                    value={ currency }
+                    key={ coin }
+                    value={ coin }
                   >
-                    { currency }
+                    { coin }
                   </option>
                 ))
               }
@@ -48,39 +99,51 @@ class Wallet extends React.Component {
           <label htmlFor="method">
             Método de Pagamento:
             <select
+              value={ method }
               id="method"
               data-testid="method-input"
+              onChange={ this.onInputChange }
             >
-              <option value="Dinheiro">Dinheiro</option>
-              <option value="Cartão de crédito">Cartão de crédito</option>
-              <option value="Cartão de débito">Cartão de débito</option>
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
             </select>
           </label>
 
-          <label htmlFor="category">
+          <label htmlFor="tag">
             Categoria:
             <select
-              id="category"
+              value={ tag }
+              id="tag"
               data-testid="tag-input"
+              onChange={ this.onInputChange }
             >
-              <option value="Alimentação">Alimentação</option>
-              <option value="Lazer">Lazer</option>
-              <option value="Trabalho">Trabalho</option>
-              <option value="Transporte">Transporte</option>
-              <option value="Saúde">Saúde</option>
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
             </select>
           </label>
 
           <label htmlFor="description">
             Descrição:
             <input
+              value={ description }
               id="description"
+              type="text"
               placeholder="Descrição"
               data-testid="description-input"
+              onChange={ this.onInputChange }
             />
           </label>
-          <input type="submit" value="Adicionar" />
-        </form>
+
+          <input
+            type="button"
+            value="Adicionar despesa"
+            onClick={ this.saveOnRedux }
+          />
+        </section>
       </>
     );
   }
@@ -91,9 +154,9 @@ Wallet.propTypes = {
   currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-function mapStateToProps(state) {
+function mapStateToProps({ wallet }) {
   return {
-    currencies: state.wallet.currencies,
+    currencies: wallet.currencies,
   };
 }
 
