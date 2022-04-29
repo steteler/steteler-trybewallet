e4ddf352db94af23cee9d357fee3cfc63049c795
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 20:38:22 -0300

    add new action 'saveCurrencies' and refatored action 'saveWallet'

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,4 +1,5 @@
 export const INPUT_LOGIN = 'INPUT_LOGIN';
+export const INPUT_CURRENCIES = 'INPUT_CURRENCIES';
 export const INPUT_WALLET = 'INPUT_WALLET';
 
 export function saveLogin(email, password) {
@@ -9,10 +10,22 @@ export function saveLogin(email, password) {
   };
 }
 
-export function saveWallet(currencies, expenses) {
+export function saveCurrencies(currencies) {
+  return {
+    type: INPUT_CURRENCIES,
+    currencies,
+  };
+}
+
+export function saveWallet(obj) {
   return {
     type: INPUT_WALLET,
-    currencies,
-    expenses,
+    id: obj.id,
+    value: obj.value,
+    currency: obj.currency,
+    method: obj.method,
+    tag: obj.tag,
+    description: obj.description,
+    exchangeRates: obj.exchangeRates,
   };
 }
