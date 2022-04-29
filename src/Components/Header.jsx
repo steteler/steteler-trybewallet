066b3b8ad17da27edef120119ffa5c4dc47b3540
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 20:39:06 -0300

    add in header the sum of expenses

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,23 @@
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
   render() {
-    const { email } = this.props;
+    const { email, expenses } = this.props;
     return (
       <header>
         <p data-testid="email-field">{ email }</p>
-        <p data-testid="total-field">0</p>
+        <p data-testid="total-field">
+          {
+            expenses.length ? (
+              expenses.reduce((acc, { value, currency, exchangeRates }) => {
+                acc += Number(value) * Number(exchangeRates[currency].ask);
+                return acc;
+              }, 0).toFixed(2)
+            ) : 0
+          }
+        </p>
         <p data-testid="header-currency-field">BRL</p>
       </header>
     );
@@ -16,12 +25,14 @@ class Header extends React.Component {
 }
 
 Header.propTypes = {
-  email: propTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
 
-function mapStateToProps(state) {
+function mapStateToProps({ user, wallet }) {
   return {
-    email: state.user.email,
+    email: user.email,
+    expenses: wallet.expenses,
   };
 }
 
