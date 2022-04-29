827b043a1d3494077b30e9f95deb7005511dafb7
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 20:37:31 -0300

    add new case for reducer wallet

--- src/reducers/wallet.jsx
+++ src/reducers/wallet.jsx
@@ -1,17 +1,34 @@
-import { INPUT_WALLET } from '../actions';
+import { INPUT_CURRENCIES, INPUT_WALLET } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
 };
 
-function wallet(state = INITIAL_STATE, { type, currencies, expenses }) {
-  switch (type) {
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case INPUT_CURRENCIES:
+    return {
+      ...state,
+      currencies: action.currencies,
+    };
+
   case INPUT_WALLET:
     return {
       ...state,
-      currencies,
-      expenses,
+      expenses:
+        [
+          ...state.expenses,
+          {
+            id: action.id,
+            value: action.value,
+            currency: action.currency,
+            method: action.method,
+            tag: action.tag,
+            description: action.description,
+            exchangeRates: action.exchangeRates,
+          },
+        ],
     };
 
   default:
