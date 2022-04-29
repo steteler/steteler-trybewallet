36046fab4a5014ee123544eb34b14ae2065f4f94
kikutii <joaovictorkikuti@gmail.com>
2022-04-29 20:36:43 -0300

    refatored 'getCurrencies' to return only API response without key 'USDT'

--- src/services/getCurrencies.jsx
+++ src/services/getCurrencies.jsx
@@ -2,7 +2,7 @@ async function getCurrencies() {
   const results = await fetch('https://economia.awesomeapi.com.br/json/all');
   const currencies = await results.json();
   delete currencies.USDT;
-  return Object.keys(currencies);
+  return currencies;
 }
 
 export default getCurrencies;
